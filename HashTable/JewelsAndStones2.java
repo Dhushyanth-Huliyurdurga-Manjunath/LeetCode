67e45abc019168a04c09c629033915b185c879e2
Dhushyanth <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-07-04 21:54:54 +0000

    07/04/2023 easy HashMap approach

---
+++ HashTable/JewelsAndStones2.java
@@ -0,0 +1,41 @@
+import java.util.Hashtable;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        
+        int numberOfStones = 0;
+        HashMap<Character, Integer> stoneFrequency = new HashMap<>();
+        HashMap<Character, Integer> jewelFrequency = new HashMap<>();
+
+        for(int i = 0; i < stones.length(); i++){
+            stoneFrequency.put(stones.charAt(i), stoneFrequency.getOrDefault(stones.charAt(i), 0) + 1);
+        }
+
+        // for(int i = 0; i < jewels.length(); i++){
+        //     jewelFrequency.put(jewels.charAt(i), jewelFrequency.getOrDefault(jewels.charAt(i), 0) + 1);
+        // }
+
+        // System.out.println(stoneFrequency);
+        // System.out.println(jewelFrequency);
+
+        for(int i = 0; i < jewels.length(); i++){
+            if(stoneFrequency.containsKey(jewels.charAt(i)))
+                numberOfStones += stoneFrequency.get(jewels.charAt(i));
+        }
+
+        // for(char c: stoneFrequency.keySet()){
+        //     if(jewelFrequency.containsKey(c))
+        //         numberOfStones += stoneFrequency.get(c);
+        // }
+
+        // for(int i = 0; i < stones.length(); i++){
+        //     if(stones.charAt())
+        //     // for(int j = 0; j < jewels.length(); j++){
+        //     //     if(jewels.charAt(j) == stones.charAt(i))
+        //     //         numberOfStones += 1;
+        //     // }
+        // }
+
+        return numberOfStones;
+    }
+}
