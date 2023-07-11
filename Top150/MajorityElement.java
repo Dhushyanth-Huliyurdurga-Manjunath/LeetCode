d74225b05d4956d92683cd9235cc78d4aa909e6e
Dhushyanth <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-07-11 21:34:25 +0000

    07/11/2023 - easy

---
+++ Top150/MajorityElement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = (int)Math.floor(nums.length/2);
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i: nums){
+            hashMap.put(i, hashMap.getOrDefault(i, 0) + 1);
+        }
+
+        for(int i: hashMap.keySet()){
+            if(hashMap.get(i) > count)
+                return i;
+        }
+
+        return 0;
+    }
+}
