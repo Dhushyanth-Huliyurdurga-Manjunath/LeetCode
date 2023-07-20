58ba16585447b07768fe270a14fef8ab7340ecb5
Dhushyanth <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-07-20 16:50:53 +0000

    07/20/2023 - easy - hashTable - pangram

---
+++ HashTable/Pangram.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Pangram{
+    public static void main(String[] args){
+        Solution56 sol = new Solution56();
+        String senetence = "thequickbrownfoxjumpsoverthelazydog";
+
+        boolean result = sol.checkIfPangram(senetence);
+        System.out.println(result);
+    }
+}
+
+class Solution56 {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> hashSet = new HashSet<Character>();
+
+        for(int i = 0; i < sentence.length(); i++){
+            if(!hashSet.contains(sentence.charAt(i))){
+                hashSet.add(sentence.charAt(i));
+            }
+        }
+
+        return hashSet.size() == 26;
+    }
+}
