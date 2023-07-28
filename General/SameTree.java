70419a88a93bada6ef1eff6aba65c59ce4b908ba
Dhushyanth <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-07-28 18:07:11 +0000

    0728 easy same binary tree

---
+++ General/SameTree.java
@@ -0,0 +1,33 @@
+import Algorithms.Node;
+
+public class SameTree {
+     public static void main(String[] args){
+        Solution63 sol = new Solution63();  
+        Node treeP = new Node();
+        Node treeQ = new Node();
+
+        treeP = treeP.insertValue(1);
+        treeP.insertValue(2);
+        treeP.insertValue(3);
+
+        treeQ = treeQ.insertValue(1);
+        treeQ.insertValue(2);
+        treeQ.insertValue(3);
+
+        boolean result = sol.isSameTree(treeP, treeQ);
+        System.out.println(result);
+
+    }    
+}
+
+class Solution63 {
+    public boolean isSameTree(Node p, Node q) {
+    
+        if(p == null && q == null)
+            return true;
+        if(p == null || q == null || p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }   
+}
