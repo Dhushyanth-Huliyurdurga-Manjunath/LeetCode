a0a7f457a08b32492bfc5a11951fb96c53a55dcc
Dhushyanth <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-08-03 17:03:57 +0000

    Letter Combination of a Phone Number #17 - medium

--- Streak/CombinationOfPhoneNumber.java
+++ Streak/CombinationOfPhoneNumber.java
@@ -1,5 +1,18 @@
-
 import java.util.*;
+
+/**
+ * @author: Dhushyanth Huliyurdurga Manjunath
+ * @since: 8/3/23 12:00 PM
+ */
+
+ /*
+  * Given a string containing digits from 2-9 inclusive,
+  * return all possible letter combinations that the number
+  * could represent. Return the answer in any order.
+  * A mapping of digits to letters (just like on the telephone buttons)
+  * is given below. Note that 1 does not map to any letters.
+  */
+
 public class CombinationOfPhoneNumber{
     public static void main(String[] args){
         Solution67 sol = new Solution67();
