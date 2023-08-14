bf35354d6c8fdfe843bcbbea75168526635b8b31
Dhushyanth <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-08-14 18:26:52 +0000

    Excel title to nubmer conversion

---
+++ General/ExcelColumnTitle.java
@@ -0,0 +1,35 @@
+package General;
+
+/**
+ * @author: Dhushyanth Huliyurdurga Manjunath
+ * @since: 8/14/23 1:23 AM
+ */
+
+ /**
+  * Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+  */
+
+  public class ExcelColumnTitle{
+    public static void main(String[] args){
+        Solution77 sol = new Solution77();
+        String columnTitle = "GITHUB";
+
+        int result = sol.titleToNumber(columnTitle);
+        System.out.println(result);
+    }
+  }
+
+  class Solution77 {
+    public int titleToNumber(String columnTitle) {
+        int result = 0; 
+        int idx = 1; 
+
+        for(int i = columnTitle.length() - 1; i >= 0; i--){
+            char c = columnTitle.charAt(i);
+            result += (c - 'A' + 1) * idx;
+            idx *= 26;
+        }   
+
+        return result; 
+    }
+}
